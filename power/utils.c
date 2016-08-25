165bdc25a45a5da71b868e64dccc75df07fe2607
SohamJ <soham4jam@gmail.com>
2016-08-25 22:57:22 +0530

    hide more logspam
    Change-Id: I3ce786ced5251909007644666c2d5a9564500469

--- power/utils.c
+++ power/utils.c
@@ -270,7 +270,7 @@ void interaction(int duration, int num_args, int opt_list[])
         if (perf_lock_acq) {
             lock_handle = perf_lock_acq(lock_handle, duration, opt_list, num_args);
             if (lock_handle == -1)
-                ALOGE("Failed to acquire lock.");
+                //ALOGE("Failed to acquire lock.");
         }
     }
 }
