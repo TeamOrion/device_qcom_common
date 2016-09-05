f28fa1f0ecaa41d5bc6b9c19125ad15abf36498c
GrinningFerret <luis.hage@outlook.de>
2016-09-05 17:02:19 +0200

    Fix commenting @Sohamj
    Change-Id: I2cc1c4d42673c62279d6745e036cd75c53dec99e
    Signed-off-by: GrinningFerret <luis.hage@outlook.de>

--- power/utils.c
+++ power/utils.c
@@ -266,14 +266,14 @@ void interaction(int duration, int num_args, int opt_list[])
     if (duration <= 0 || num_args < 1 || opt_list[0] == 0)
         return;
 
- /**
+ /*
     if (qcopt_handle) {
         if (perf_lock_acq) {
             lock_handle = perf_lock_acq(lock_handle, duration, opt_list, num_args);
-            if (lock_handle == -1)            	
-                //ALOGE("Failed to acquire lock.");
-            /* if (lock_handle == -1)
-               ALOGE("Failed to acquire lock."); */
+            if (lock_handle == -1)
+                ALOGE("Failed to acquire lock.");
+            if (lock_handle == -1)
+               ALOGE("Failed to acquire lock.");
         }
     }
  */
